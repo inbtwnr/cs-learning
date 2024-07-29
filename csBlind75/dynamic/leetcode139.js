2be5bc585f5b17c1953e2fa0492e383b3f7f217d
alexander <alexanderjuror@gmail.com>
2024-07-29 21:03:19 +0300

    feat: added  part of solution to the 139 problem

---
+++ csBlind75/dynamic/leetcode139.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    const dp = new Array(s.length + 1).map(() => false)
+
+    
+    for (let i = s.length; i > 0; i--) {
+        for (let w of wordStr) {
+            if ((i + w.lenght) <= s.length)
+        }      
+    }
+};
