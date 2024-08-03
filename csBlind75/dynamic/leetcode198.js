ffc0444a924e2a459c28e6faf761a280134921dc
alexander <alexanderjuror@gmail.com>
2024-08-03 15:06:59 +0300

    198 problem on leetcode

---
+++ csBlind75/dynamic/leetcode198.js
@@ -0,0 +1,12 @@
+export const rob = function(nums) {
+    let rob1 = 0;
+    let rob2 = 0;
+
+    for (let n of nums) {
+        let temp = Math.max(n  + rob1, rob2);
+        rob1 = rob2;
+        rob2 = temp
+    }
+
+    return rob2;
+};
